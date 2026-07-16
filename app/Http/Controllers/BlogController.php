cb1e930306f0464762c028afe737ef0380265890
jaydevthapaa <jaydevthapa8@gmail.com>
2026-07-16 20:19:52 +0545

    feat: implement blog listing and creation with image uploads

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Blog;
 class BlogController extends Controller
 {
+    function index(){
+        $blogs = Blog::latest()->get();
+
+    return view('blogs.index', compact('blogs'));
+    }
+
+    function create(){
+        return view('blogs.create');
+    }
+
+    function store(Request $request){
+        $request->validate([
+        'title' => 'required',
+        'description' => 'required',
+        'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
+    ]);
+
+    $imageName = null;
+
+    if ($request->hasFile('image')) {
+
+        $imageName = $request->file('image')->store('blogs', 'public');
+
+    }
+
+    Blog::create([
+        'title' => $request->title,
+        'description' => $request->description,
+        'image' => $imageName
+    ]);
+
+    return redirect('/blogs');
+    }
     
 }
