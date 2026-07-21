9b3768ed5740afeb1090ed2797b593f32b9de5ec
jaydevthapaa <jaydevthapa8@gmail.com>
2026-07-21 10:32:32 +0545

    feat: added function to disply blog and blogdetail page by featching from database

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Blog;
+
+
+class FrontedController extends Controller
+{
+    function events(){
+        $events = Event::where('status', 'Upcoming') -> orderBy('event_date', 'asc')-> paginate(9);
+
+        return view('event', compact('events'));
+    }
+
+    function eventDetail(int $id){
+        $event = Event::findOrFail($id);
+
+        $reletedEvents= Event::where('id', '!=', $id)->where('status', 'Upcoming') ->latest() -> take(3) -> get();
+
+        return view('event_detail', compact('event', 'reletedEvents'));
+    }
+
+    // blog functiions
+    function blogs(){
+        $blogs = Blog::latest()->get();
+
+        return view('blog', compact('blogs'));
+    }
+
+    function blogDetail(int $id){
+        $blog = Blog::findORFail($id);
+        return view('blog_detail', compact('blog'));
+    }
+    
+}
